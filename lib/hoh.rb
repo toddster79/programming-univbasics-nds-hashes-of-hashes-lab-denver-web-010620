0376564afe4008ee97186691dd14ff28284025ed
Todd Carlson <todd.carlson3030@me.com>
2019-12-22 20:14:18 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -52,7 +52,13 @@ def naming_system
                   :sub_category=> {
                     :label=> "Family",
                       :sub_category=> {
-                        :label=> "Genus"
+                        :label=> "Genus",
+                          :sub_category=> {
+                            :label=> "Species",
+                            :sub_category=> {
+                              :label=> "nil"
+                            }
+                          }
                       }
                   }
               }
