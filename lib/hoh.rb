4c2461ff2925c526a9df5256a7a9e43bdc6a1aa2
Todd Carlson <todd.carlson3030@me.com>
2019-12-22 20:15:40 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,10 +55,7 @@ def naming_system
                         :label=> "Genus",
                           :sub_category=> {
                             :label=> "Species",
-                            :sub_category=> {
-                              :label=> "nil"
-                            }
-                          }
+                            :sub_category=> nil 
                       }
                   }
               }
